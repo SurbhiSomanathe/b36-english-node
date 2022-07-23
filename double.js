2ffa718f771a13eee49edcc94748370b491029dc
surbhisomnathe <surbhisomnathe7777@gmail.com>
2022-07-23 22:46:04 +0530

    node

--- double.js
+++
@@ -1,5 +0,0 @@
-const dbl = (num) => num * 2;
-
-console.log (dbl(10));
-console.log(global);
-console.log ("hello world");
--- spec.js
+++
@@ -1,9 +0,0 @@
-// const os = require ("os");
-
-// console.log("Free memory in GB" , os.freemem() / 1024 / 1024 / 1024);
-// console.log("Total memory in GB", os.totalmem() / 1024 / 1024 / 1024);
-// console.log("version" , os.version());
-// console.log("cpus", os.cpus());
-
-const dbl = (num) => num1 + num2;
-console.log(dbl(num));
